4f26739c08dbfec480b6f8fbf6f826dafdfbfd1b
unknown <Ryan@Charizard-PC.(none)>
2013-07-06 22:13:13 -0400

    Initing game

--- src/game.js
+++ src/game.js
@@ -1,5 +1,9 @@
 Game = {
-	start: function(){
-		
+	width: 	function() { return 450; },
+	height: function() { return 800; },
+	start: 	function(){
+		//Init crafty
+		Crafty.init(Game.width(), Game.height());
+		Crafty.background('black');
 	}
 }
