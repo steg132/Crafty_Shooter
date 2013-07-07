594c330397de0832934f71dcc23675a23f2dfc68
Ryan Schumacher <steg132@gmail.com>
2013-07-07 14:10:06 -0400

    Added Player firing

--- src/components.js
+++ src/components.js
@@ -7,36 +7,85 @@ Crafty.c('Actor', {
   at: function(x, y) {
   	if (x === undefined && y === undefined) {
   		return {x: this.x + this.w/2, y: this.y + this.h / 2};
-  	} else {
-  		this.x = x - this.w / 2;
-  		this.y = y - this.h / 2;
-  		return this;
-  	}
+  	} else if ( x.x !== undefined && x.y !== undefined ) {
+  		// check if the first input is an object
+  	  	y = x.y;
+	  	x = x.x; 
+	} 
+  	this.x = x - this.w / 2;
+  	this.y = y - this.h / 2;
+  	
+  	return this;
   }
 });
 
 Crafty.c('Player', {
+	isFiring: false,
 	init: function() {
-		this.requires('Actor, Twoway, Image')
+		this.requires('Actor, Twoway, Image, Delay')
 		.attr({w:64, h:52})
 		.image('assets/spaceship1.png')
 		// Two way movement w/o jump
-		.twoway(5, 0);
+		.twoway(5, 0)
 
 		// Bind Events
-		this.bind('Moved', function(data) {
+		// Check when the player moved
+		.bind('Moved', function(data) {
 			// check bounds
 			if ( this.x < 0)
 				this.x = 0;
 			if ( (this.x + this.w) > Game.width() )
 				this.x = Game.width() - this.w;
+		})
+		// Get key input for player
+		.bind('KeyDown', function(e) {
+			if(e.key == Crafty.keys['SPACE']) {
+				// start Firing
+				this.isFiring = true;
+				// start a delay
+				this.fire();
+			}
+		})
+		.bind('KeyUp', function(e) {
+			if(e.key == Crafty.keys['SPACE']) {
+				// done firing
+				this.isFiring = false;
+			}
+		})
+		// Update player every frame
+		.bind('EnterFrame', function() {
+		});
+	},
+	fire: function() {
+		// check if we are firing
+		if ( this.isFiring == false ) 
+			return;
+		// fire a bullet
+		Crafty.e('Bullet').at(this.at())
+		.attr({velocity: {x:0, y:-7}});
 
+		// schedule the next firing
+		this.delay(this.fire, 250, 0);
+	}
+});
+
+Crafty.c('Projectile', {
+	velocity:{x:0, y:0},
+	init: function() {
+		this.requires("Actor")
+		.bind("EnterFrame", function() {
+			this.x += this.velocity.x;
+			this.y += this.velocity.y;
+			// Check if we are out of frame
+			if ( this.y < (-1 * this.h) ) 
+				this.destroy();
 		});
 	}
 });
 
 Crafty.c('Bullet', {
 	init: function() {
-		
+		this.requires("Actor, Projectile, spr_blt0")
+		.attr({w:39, h:39});
 	}
 });
--- src/game.js
+++ src/game.js
@@ -6,6 +6,12 @@ Game = {
 		//Init crafty
 		Crafty.init(Game.width(), Game.height());
 		Crafty.background('black');
+		// Load Sprites
+		Crafty.sprite(39, 'assets/spr_bullet_strip.png', {
+			spr_blt0: [0, 0],
+			spr_blt1: [1, 0],
+			spr_blt2: [2, 0]
+		});
 		// add the player object to the game
 		this.player = Crafty.e('Player').at(Game.width() / 2, Game.height() - 100);
 	}
