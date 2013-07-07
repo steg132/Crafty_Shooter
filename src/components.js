681c387f10975a2a1333271cc869f9097c920318
unknown <Ryan@Charizard-PC.(none)>
2013-07-06 23:05:17 -0400

    Added basic player to project

--- src/components.js
+++ src/components.js
@@ -4,4 +4,33 @@ Crafty.c('Actor', {
   init: function() {
     this.requires('2D, DOM');
   },
+  at: function(x, y) {
+  	if (x === undefined && y === undefined) {
+  		return {x: this.x + this.w/2, y: this.y + this.h / 2};
+  	} else {
+  		this.x = x - this.w / 2;
+  		this.y = y - this.h / 2;
+  		return this;
+  	}
+  }
+});
+
+Crafty.c('Player', {
+	init: function() {
+		this.requires('Actor, Twoway, Image')
+		.attr({w:64, h:52})
+		.image('assets/spaceship1.png')
+		// Two way movement w/o jump
+		.twoway(5, 0);
+
+		// Bind Events
+		this.bind('Moved', function(data) {
+			// check bounds
+			if ( this.x < 0)
+				this.x = 0;
+			if ( (this.x + this.w) > Game.width() )
+				this.x = Game.width() - this.w;
+
+		});
+	}
 });
--- src/game.js
+++ src/game.js
@@ -1,9 +1,12 @@
 Game = {
 	width: 	function() { return 450; },
 	height: function() { return 800; },
+	player: null,
 	start: 	function(){
 		//Init crafty
 		Crafty.init(Game.width(), Game.height());
 		Crafty.background('black');
+		// add the player object to the game
+		this.player = Crafty.e('Player').at(Game.width() / 2, Game.height() - 100);
 	}
 }
